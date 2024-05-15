d1fb89aeeb08d0fc43e03fb0cfca7ba731bc34c5
chandragauravsharma <chandragauravsharma@gmail.com>
2024-05-15 14:49:50 +0530

    Initial commit : project setup and adding readme

---
+++ src/main/java/com/mktplace/MktplaceApp.java
@@ -0,0 +1,11 @@
+package com.mktplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktplaceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MktplaceApp.class, args);
+    }
+}
