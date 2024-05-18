d34a398f460c3958fb97e84162ee74ad03da655d
chandragauravsharma <chandragauravsharma@gmail.com>
2024-05-18 16:42:40 +0530

    changes to enable swagger config

--- src/main/java/com/mktplace/api/impl/BookingApiDelegateImpl.java
+++ src/main/java/com/mktplace/api/impl/BookingApiDelegateImpl.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 public class BookingApiDelegateImpl implements BookingApiDelegate {
     @Autowired
     BookingService bookingService;
--- src/main/java/com/mktplace/api/impl/ProductApiDelegateImpl.java
+++ src/main/java/com/mktplace/api/impl/ProductApiDelegateImpl.java
@@ -9,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 public class ProductApiDelegateImpl implements ProductApiDelegate {
     @Autowired
     ProductService productService;
